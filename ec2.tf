3cf7b357031015eb9b0f3d9a668c706137d29531
mounihubgit <eatstreet.aw@gmail.com>
2023-05-25 06:52:59 +0000

    commit

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "lms-web-server" {
   ami                    = "ami-0f8ca728008ff5af4"
   instance_type          = "t2.micro"
-  key_name               = "ravi"
+  key_name               = "mouni"
   subnet_id              = aws_subnet.lms-subnet.id
   vpc_security_group_ids = [aws_security_group.lms-web-sg.id]
   user_data              = file("web.sh")
