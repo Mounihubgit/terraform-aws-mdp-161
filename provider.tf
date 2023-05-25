5627cc30ad9bb2ae2d174f940476d26332903446
mounihubgit <eatstreet.aw@gmail.com>
2023-05-25 06:44:15 +0000

    commit

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
   access_key = "AKIAWWI4OMZG4MTOG7GV"
-  secret_key = "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
+  secret_key = "Nr28dVGCpDYewDCG/JYV72QKmEzCB5x7JzRI/hoT"
 }
